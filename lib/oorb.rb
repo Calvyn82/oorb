374ecf909c840f832cd1c0c417f64c7aaa17bc6b
Calvyn82 <claytonflesher@gmail.com>
2016-06-11 10:08:32 -0500

    Updated version included oorb.rb updates for last commit

--- lib/oorb.rb
+++ lib/oorb.rb
@@ -1,74 +1,71 @@
 require "oorb/version"
-LETTERS = {'s' => %w(f l j i 3 8 5),
-           'h' => %w(b),
-           'b' => %w(h),
-           'y' => %w(v j 7),
-           'c' => %w(e f d o 6),
-           'i' => %w(l 1),
-           'e' => %w(6 c d f 4 3),
-           'o' => %w(c 6 0 3 d),
-           't' => %w(i l 4),
-           'a' => %w(9),
-           'l' => %w(1 i t),
-           'v' => %w(y),
-           'f' => %w(c s),
-           'd' => %w(3 0 o),
-           'z' => %w(2),
-           'g' => %w(9 8),
-           'j' => %w(y),
-}
 
 ##
 # OCR Optimized Regex Builder
 class OORB
-  def initialize
-    @letters = LETTERS
-  end
 
-  attr_reader :letters
+  ##
+  # Letters that regularly are mistaken in OCR and their common replacements
+  LETTERS = {'s' => %w(f l j i 3 8 5),
+             'h' => %w(b),
+             'b' => %w(h),
+             'y' => %w(v j 7),
+             'c' => %w(e f d o 6),
+             'i' => %w(l 1),
+             'e' => %w(6 c d f 4 3),
+             'o' => %w(c 6 0 3 d),
+             't' => %w(i l 4),
+             'a' => %w(9),
+             'l' => %w(1 i t),
+             'v' => %w(y),
+             'f' => %w(c s),
+             'd' => %w(3 0 o),
+             'z' => %w(2),
+             'g' => %w(9 8),
+             'j' => %w(y),
+  }
 
   ##
   # Runs the application from the command line
   def run
     puts "Waiting for a statement."
     user_input = gets.chomp
-    puts build_regex(user_input)
+    combined = combine_whitespace(user_input)
+    puts build_regex(combined)
     run
   end
 
   ##
   # Builds an OCR optimized regular expression from a string
   def build_regex(input)
-    if input =~ /[\[\]]/
-      raise ArgumentError, "Square braces? Really!? Fuck off."
-    end
-    statement = input.downcase.chars.map do |l|
-      LETTERS.has_key?(l) ? build_collection(l) : l
+    input.downcase.chars.map do |char|
+      LETTERS.has_key?(char) ? build_collection(char) : escape(char)
     end.join
-    escapes(statement)
+  end
+
+  ##
+  # Combines whitespace characters to make "   " into " "
+  def combine_whitespace(string)
+    string.gsub(/\s+/, "\s")
   end
 
   ##
   # Builds a group match from an input letter
   # Raises an argument error if the letter isn't from the LETTERS hash
-  def build_collection(letter)
-    unless LETTERS[letter]
-      raise ArgumentError, "Valid arguments are #{LETTERS.keys.join(", ")}."
+  def build_collection(character)
+    unless LETTERS[character]
+      raise ArgumentError, "Valid arguments are a single character from #{LETTERS.keys.join(", ")}."
     end
-    LETTERS[letter].each { |x| letter << x }
-    "[#{letter}]"
+    LETTERS[character].each { |x| character << x }
+    "[#{character}]"
   end
 
   ##
-  # Escapes backslashes, whitespace, punctuation, parens and curly braces
-  # Square brackets are forbidden
-  def escapes(string)
-    string.gsub(/\\/, "\\\\\\")          # escape backslashes
-      .gsub(/\s+/, "\\s")                # make whitespace \s
-      .gsub(/[.,;:'`]/, "[.,;:'`\\s]?")  # make punction optional
-      .gsub(/\(/, "\\(")                 # escape open parens
-      .gsub(/\)/, "\\)")                 # escape close parens
-      .gsub(/\{/, "\\{")                 # escape open curly braces
-      .gsub(/\}/, "\\}")                 # escape open curly braces
+  # Escapes a single-character string and makes \s optional \\s?
+  def escape(character)
+    if character.length > 1
+      raise ArgumentError, "Argument must be a single character string"
+    end
+    character == "\s" ? "\\s?" : Regexp.escape(character)
   end
 end
